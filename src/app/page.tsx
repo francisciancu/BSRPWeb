94a35c8c4a35358062235a0646dadcdb0e9caeff
Francisc Andrei Sebastian Iancu <andreiiancu14@gmail.com>
2024-09-21 23:06:43 +0300

    hope45

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,39 @@
-import MainHero from '@/components/heros/main-hero';
-import AboutSection from '@/sections/about-section';
-import ServerNetworkSection from '@/sections/server-network-section';
-import { config } from '../../config';
-import dynamic from 'next/dynamic';
-import { LeaderboardEntry, getServerLeaderboards } from '@/lib/cftools';
-import ContactSection from '@/sections/contact-section';
+import MainHero from "@/components/heros/main-hero";
+import AboutSection from "@/sections/about-section";
+import ServerNetworkSection from "@/sections/server-network-section";
+import { config } from "../../config";
+import dynamic from "next/dynamic";
+import { LeaderboardEntry, getServerLeaderboards } from "@/lib/cftools";
+import ContactSection from "@/sections/contact-section";
+import TestStuffSection from "@/sections/test-stuff-section";
 
 // Lazy load chonky leaderboard section
-const LeaderboardSection = dynamic(() => import('@/sections/leaderboard-section'));
+const LeaderboardSection = dynamic(
+  () => import("@/sections/leaderboard-section")
+);
 
 export default async function Home() {
-  let data = config.cftools.leaderboard.enabled ? await getServerLeaderboards() : {};
-  let error = 'error' in data
-    ? data.error as string
-    : null;
+  let data = config.cftools.leaderboard.enabled
+    ? await getServerLeaderboards()
+    : {};
+  let error = "error" in data ? (data.error as string) : null;
   if (error) data = {};
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between w-full'>
+    <main className="flex min-h-screen flex-col items-center justify-between w-full">
       <MainHero />
       <AboutSection />
+      <TestStuffSection />
       <ServerNetworkSection />
-      {config.cftools.leaderboard.enabled && <LeaderboardSection data={data as {
-        [k: string]: LeaderboardEntry[];
-      }} error={error} />}
+      {config.cftools.leaderboard.enabled && (
+        <LeaderboardSection
+          data={
+            data as {
+              [k: string]: LeaderboardEntry[];
+            }
+          }
+          error={error}
+        />
+      )}
       <ContactSection />
     </main>
   );
