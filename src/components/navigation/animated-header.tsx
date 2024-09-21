890a70d30d50570655057ca46e224952ec598bb9
Francisc Andrei Sebastian Iancu <andreiiancu14@gmail.com>
2024-09-22 02:27:52 +0300

    test

--- src/components/navigation/animated-header.tsx
+++ src/components/navigation/animated-header.tsx
@@ -17,15 +17,15 @@ import { cn } from "@/lib/utils";
 
 export const defaultNavigation = {
   items: config.navigation?.items ?? [
-    { label: "About", href: "#about", enabled: true },
-    { label: "Servers", href: "#servers", enabled: true },
+    { label: "About", href: "/#about", enabled: true },
+    { label: "Servers", href: "/#servers", enabled: true },
     {
       label: "Leaderboard",
       href: "#leaderboard",
       enabled: config.cftools.leaderboard.enabled,
     },
     { label: "Crafting Guide", href: "/Crafting_Guide", enabled: true },
-    { label: "Contact", href: "#contact", enabled: true },
+    { label: "Contact", href: "/#contact", enabled: true },
   ],
 } satisfies { items: NavItem[] };
 
